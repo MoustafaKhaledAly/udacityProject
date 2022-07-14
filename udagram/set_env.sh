e926fdee1b1ffc4a1b08b5574a5e2e4029eceefe
MoustafaAlyy <moustafa.khaled.aly@gmail.com>
2022-07-14 12:23:00 +0200

    first commit

---
+++ udagram/set_env.sh
@@ -0,0 +1,13 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=postgres
+export POSTGRES_HOST=database-1.cvbharnigbez.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=arn:aws:s3:::myawsbucket-75139724085
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export AWS_ACCESS_KEY_ID=AKIASLD3EOK74LN5UQDW
+export AWS_SECRET_ACCESS_KEY=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
+export JWT_SECRET=mysecretstring
+export URL=http://localhost:8100
