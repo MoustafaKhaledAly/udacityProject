e9988c5f64deff26069229bee60f07c6e6c1a36e
MoustafaAlyy <moustafa.khaled.aly@gmail.com>
2022-07-14 16:06:51 +0200

    first commit

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: "Udagram",
-  apiHost: "http://localhost:8080/api/v0",
+  apiHost: "http://projectenvv-env.eba-xsukr2pt.us-east-1.elasticbeanstalk.com/",
 };
 
 /*
--- udagram/udagram-frontend/src/environments/environment.ts
+++ udagram/udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://projectenvv-env.eba-xsukr2pt.us-east-1.elasticbeanstalk.com/'
 };
 
 /*
