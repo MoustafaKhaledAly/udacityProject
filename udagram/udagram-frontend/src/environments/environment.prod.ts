682cca7f5a5a466b52411a8b8b586437cebee675
MoustafaAlyy <moustafa.khaled.aly@gmail.com>
2022-07-15 11:25:00 +0200

    to submit

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: "Udagram",
-  apiHost: "http://projectfinal-env.eba-jsn6cp5d.us-east-1.elasticbeanstalk.com/api/v0",
+  apiHost: "http://projectfinal-env.eba-93qzt3mj.us-east-1.elasticbeanstalk.com/api/v0",
 };
 
 /*
--- udagram/udagram-frontend/src/environments/environment.ts
+++ udagram/udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://projectfinal-env.eba-jsn6cp5d.us-east-1.elasticbeanstalk.com/api/v0'
+  apiHost: 'http://projectfinal-env.eba-93qzt3mj.us-east-1.elasticbeanstalk.com/api/v0'
   
 };
 
